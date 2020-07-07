4c40b3743d1e21eab69c2456f265c35f0ca768e0
kndwin <tnkevino@gmail.com>
2020-07-07 23:44:37 +1000

    Rendered pug!

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const pug = require("pug");
 
 const app = express();
 
@@ -10,9 +11,12 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
+
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  // process.cwd means current working directory
+  res.render( process.cwd() + '/views/pug/index.pug' ) ;
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
