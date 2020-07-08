11a7e2fbb84b47cfabc34d3be9f0376100f72ff6
kndwin <tnkevino@gmail.com>
2020-07-08 19:22:11 +1000

    Made a session!

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const pug = require("pug");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -10,14 +12,19 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session({ // you can create sessions (useful to auth later)
+  secret: 'itsgonnabeokay',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
 app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   // process.cwd() means current working directory
   // __dirname
   res.render( __dirname + '/views/pug/index.pug' , { 
-    title: "Hello",
+    title: "Hello", // You can pass variables to .pug files!
     message: "Please login"
   }) ;
 
