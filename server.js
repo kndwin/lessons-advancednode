9850fbb41abc904c5ad212406d008d41c39d1b37
kndwin <tnkevino@gmail.com>
2020-07-13 20:06:59 +1000

    Added app.use(passport.session()) The documentation doesn't mentioned why yet but it seems this is the new way to do it

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const pug = require("pug");
 const session = require("express-session");
 const passport = require("passport");
-//require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 
@@ -13,18 +13,21 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+
 app.use(session({ // you can create sessions (useful to auth later)
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true
 }))
+
 app.use(passport.initialize());
-app.set('view engine', 'pug');
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   // process.cwd() means current working directory
-  // __dirname
-  res.render( __dirname + '/views/pug/index.pug' , { 
+  res.render( process.cwd() + '/views/pug/index.pug' , { 
     title: "Hello", // You can pass variables to .pug files!
     message: "Please login"
   }) ;
