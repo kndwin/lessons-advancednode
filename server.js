ef6eb5a16edb3f1555e749e8d52f218b5ee9d615
kndwin <tnkevino@gmail.com>
2020-07-18 22:03:41 +1000

    Added a strategy (passport-local)

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
 const mongo = require('mongodb').MongoClient;
+const LocalStrategy = require('passport-local');
 
 if (process.env.NODE_ENV= 'production') {
   require('dotenv').config();
@@ -49,6 +50,18 @@ mongo.connect(process.env.DATABASE, (err, db) => {
         ( err, doc ) => { done(null,doc) }
       )
     })
+
+    passport.use(new LocalStrategy(
+      function( username, password, done ) {
+        db.collection('users').findOne({ username: username }, function (err, user) {
+          console.log('User ' + username + ' attempted to log in.');
+          if (err) { return done(err) }
+          if (!user) { return done(null, false) }
+          if (password !== user.password) { return done(null, false) }
+          return done( null, user )
+        })
+      }
+    ))
 
     app.route("/").get((req, res) => {
       // process.cwd() means current working directory
