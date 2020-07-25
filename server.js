4c016d1a34211ae802bfaa205507d59cf6a4b048
kndwin <tnkevino@gmail.com>
2020-07-25 16:04:42 +1000

    Added debug logs for err in /register

--- server.js
+++ server.js
@@ -97,37 +97,41 @@ mongo.connect(process.env.DATABASE, (err, db) => {
 
     app.route('/profile').get(
       ensureAuthenticated,(req, res) => {
-        console.log(req.user);
         res.render(process.cwd() + '/views/pug/profile', {
           username: req.user.username
         })
       }
     )
     
-    app.route('/register').post(
-      (req, res, next) => {
+    app.route('/register')
+      .post((req, res, next) => {
         db.collection('users').findOne({ username: req.body.username }), (err, user) => {
         if (err) {
           next(err)
         } else if (user) {
           res.redirect('/')
-          } else {
+        } else {
           db.collection('users').insertOne({
             username: req.body.username,
             password: req.body.password
-          }, (err, doc) => {
-            if (err) {
-              res.redirect('/')
-            } else {
-              next(null, user)
+          }, 
+            (err, doc) => {
+              if (err) {
+                console.log(`Error: ${err}`);
+                res.redirect('/')
+              } else {
+                next(null, user)
+              }
             }
-          })
+          )
         }
-      },passport.authenticate('local', { failureRedirect: '/' })
-      ,(req, res, next) => { 
-        res.redirect('/profile') 
-      }
-    })
+      },
+        passport.authenticate('local', { failureRedirect: '/' }),
+          (req, res, next) => { 
+            res.redirect('/profile') 
+          }
+        }
+      )
 
     app.route('/logout').get((req, res) => {
       req.logout()
