001cf78660344dd76dbd06f6274d53b3e2c67dea
Kevin Nguyen <me@kndwin.dev>
2020-09-20 15:36:28 +1000

    🔨 fixed bcrypt.comapreSync())

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ mongo.connect(process.env.DATABASE, (err, client) => {
           if (!user) { 
             return done(null, false) 
           }
-          if (password !== user.password) { 
+          if (!bcrypt.compareSync(password, user.password) { 
             return done(null, false) 
           }
           return done( null, user )
