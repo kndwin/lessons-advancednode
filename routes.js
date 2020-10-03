2c50311df9ff506708b4b4ea638df6e07b146d93
Kevin Nguyen <me@kndwin.dev>
2020-10-03 22:25:13 +1000

    🐛 fcc has testing issues (need to move github endpoint to server.js

--- routes.js
+++ routes.js
@@ -72,16 +72,6 @@ module.exports = function (app, database) {
       res.redirect("/")
     })
 
-  app.route('/auth/github')
-    .get(passport.authenticate('github'))
-
-  app.route('/auth/github/callback')
-    .get(passport.authenticate('github', {
-      failureRedirect: '/'
-    }), (req, res) => {
-      res.redirect('/profile')
-    })
-
   app.use((req, res, next) => {
     res.status(404).type('text').send('Not Found');
   })
--- server.js
+++ server.js
@@ -46,6 +46,17 @@ mongo.connect(process.env.DATABASE, (err, client) => {
     console.log(`Sucessful database connection`)
     auth(app, db)
     routes(app, db)
+
+    app.route('/auth/github')
+      .get(passport.authenticate('github'))
+
+    app.route('/auth/github/callback')
+      .get(passport.authenticate('github', {
+        failureRedirect: '/'
+      }), (req, res) => {
+        res.redirect('/profile')
+      })
+
   }
 })
 
